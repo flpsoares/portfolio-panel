3c4aff8ed4bc3ed8d0a94c1d80abca4b020ca553
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-01 13:18:25 -0300

    feat: create sidear

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,14 +1,35 @@
-import { Container } from './style'
+import { Container, Logo, Wrapper } from './style'
 
-import { Link } from 'react-router-dom'
+import { ButtonSideBar } from '../ButtonSideBar'
+
+import { IoCreateOutline } from 'react-icons/io5'
+import { IoMdRemoveCircleOutline } from 'react-icons/io'
+import { AiOutlineEdit, AiOutlineHome } from 'react-icons/ai'
 
 export const SideBar: React.FC = () => {
   return (
     <Container>
-      <Link to="/">Home</Link>
-      <Link to="/create">Criar</Link>
-      <Link to="/edit">Editar</Link>
-      <Link to="/remove">Remover</Link>
+      <Logo>
+        <span>Control Panel</span>
+      </Logo>
+      <Wrapper>
+        <ButtonSideBar icon={<AiOutlineHome size={20} />} path="/" title="Home" />
+        <ButtonSideBar
+          icon={<IoCreateOutline size={20} />}
+          path="/create"
+          title="Criar"
+        />
+        <ButtonSideBar
+          icon={<AiOutlineEdit size={20} />}
+          path="/edit"
+          title="Editar"
+        />
+        <ButtonSideBar
+          icon={<IoMdRemoveCircleOutline size={20} />}
+          path="/remove"
+          title="Remover"
+        />
+      </Wrapper>
     </Container>
   )
 }
--- src/components/SideBar/style.ts
+++ src/components/SideBar/style.ts
@@ -1,14 +1,31 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  width: 400px;
+  width: 300px;
   height: 100%;
-
-  grid-area: sidebar;
-
-  border: 2px solid black;
 
   display: flex;
   flex-direction: column;
-  gap: 20px;
+
+  background: var(--primary);
+`
+
+export const Logo = styled.div`
+  text-align: center;
+
+  margin-top: 20px;
+  span {
+    color: var(--text-primary);
+    font-size: 36px;
+    font-weight: 600;
+  }
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 35px;
+
+  margin-top: 45px;
 `
