af65f0534c988c1f62a69d1f719258010fb16012
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-01 21:33:19 -0300

    feat: created project list

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react'
+
+import { Project } from '../components/Project'
 import { Title } from '../components/Title'
-import { Container } from './styles/home'
+import { api } from '../services/api'
+import { Container, Wrapper } from './styles/home'
+
+interface ProjectProps {
+  id: number
+  name: string
+}
 
 export const Home: React.FC = () => {
+  const [projects, setProjects] = useState<ProjectProps[]>([])
+
+  useEffect(() => {
+    api.get('projects').then((res) => setProjects(res.data))
+  }, [])
+
   return (
     <Container>
       <Title>Home</Title>
+      <Wrapper>
+        {projects.map((project) => {
+          return <Project key={project.id} title={project.name} />
+        })}
+      </Wrapper>
     </Container>
   )
 }
--- src/pages/styles/home.ts
+++ src/pages/styles/home.ts
@@ -3,4 +3,15 @@ import styled from 'styled-components'
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+  padding: 0 80px;
+
+  overflow-y: auto;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 40px;
 `
