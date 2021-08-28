35c3bfa80bb0ddb304e73076861b0ca508348507
Filipe Soares <filipeseventeen1@gmail.com>
2021-08-28 15:49:53 -0300

    feat: created the file to make the requests

---
+++ src/services/api/ProjectApi.ts
@@ -0,0 +1,44 @@
+import { api } from '../api'
+
+class ProjectApi {
+  public async createWithLink({
+    name,
+    description,
+    link,
+    technologies
+  }: Partial<App.Project>) {
+    return api
+      .post('project', {
+        name,
+        description,
+        link,
+        technologies
+      })
+      .then((res) => res.data)
+  }
+
+  // public async createWithImage({
+  //   name,
+  //   description,
+  //   image,
+  //   technologies
+  // }: Partial<App.Project>) {
+  //   const formData = new FormData()
+  //   formData.append('image', image)
+
+  //   return api
+  //     .post('project', {
+  //       name,
+  //       description,
+  //       image,
+  //       technologies
+  //     })
+  //     .then((res) => res.data)
+  // }
+
+  public async listTechnologies() {
+    return api.get<App.Technology>('technologies').then((res) => res.data)
+  }
+}
+
+export default new ProjectApi()
