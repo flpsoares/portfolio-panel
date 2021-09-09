411d44b905aa68c1d0cadaa98f1ae396e6b87fbd
Filipe <filipeseventeen1@gmail.com>
2021-09-09 15:07:25 -0300

    refactor: removed the undefined typing of the elements

--- src/@types/project.d.ts
+++ src/@types/project.d.ts
@@ -1,9 +1,9 @@
 declare namespace App {
   export interface Project extends App.Model {
-    name: string | undefined
-    link?: string | null
-    description: string | undefined
+    name: string
+    link?: string
+    description: string
     image?: App.Image[]
-    technologies: number[] | undefined
+    technologies: number[]
   }
 }
